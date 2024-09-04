f292129766af3384776d3e98172af73e85749354
markup54 <marzenakupka@tlen.pl>
2024-09-04 13:42:14 +0200

    losowanie liczby cakowitej

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Witaj świecie");
         System.out.println("na lekcji programowania");
 
-        double wylosowana = Math.random()*10+1;
+        int wylosowana = (int)(Math.random()*10+1);
         System.out.println(wylosowana);
     }
 }
