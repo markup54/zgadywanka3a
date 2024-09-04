71a1c966f8efe2af748253ef0b3b2a588543ae0f
markup54 <marzenakupka@tlen.pl>
2024-09-04 13:40:14 +0200

    losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj świecie");
         System.out.println("na lekcji programowania");
+
+        double wylosowana = Math.random()*10+1;
+        System.out.println(wylosowana);
     }
 }
